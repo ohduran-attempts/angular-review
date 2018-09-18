a2eb984e929a94b07cc6a8c74e2b0e3160dc105f
Alvaro Duran <alvaro.duranb@gmail.com>
2018-09-18 17:34:32 +0200

    delete functionality implemented

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -16,7 +16,7 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';
 
-  const httpOptions = {
+  private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
@@ -67,6 +67,16 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = '${this.heroesUrl}/${id}';
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+        tap(_ => this.log('deleted hero id=${id}')),
+        catchError(this.handleError<Hero>('deleteHero'))
     );
   }
 }
