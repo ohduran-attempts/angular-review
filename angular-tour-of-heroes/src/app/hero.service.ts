34619a54c4da27cac7cfc3f66a604cebbaacfd11
Alvaro Duran <alvaro.duranb@gmail.com>
2018-09-18 16:27:26 +0200

    delete commit in progress

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -16,6 +16,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';
 
+  const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   private log(message:string) {
     this.messageService.add('HeroService: ${message}')
   }
@@ -34,20 +38,35 @@ export class HeroService {
     };
   }
 
-  /** GET heroes from the server */
-getHeroes (): Observable<Hero[]> {
-  return this.http.get<Hero[]>(this.heroesUrl)
-    .pipe(
-      tap(heroes => this.log('fetched heroes')),
-      catchError(this.handleError('getHeroes', []))
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((hero: Hero) => this.log('added hero with id=${hero.id}')),
+      catchError(this.handleError<Hero>('addHero'))
     );
-}
-  /** GET hero by id. Will 404 if id not found */
-getHero(id: number): Observable<Hero> {
-  const url = `${this.heroesUrl}/${id}`;
-  return this.http.get<Hero>(url).pipe(
-    tap(_ => this.log(`fetched hero id=${id}`)),
-    catchError(this.handleError<Hero>(`getHero id=${id}`))
-  );
-}
+  }
+
+    /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+    /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log('fetched heroes')),
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+    /** GET hero by id. Will 404 if id not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
 }
